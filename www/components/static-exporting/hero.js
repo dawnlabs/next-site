7bc3091dcb6891b5b5b5e576f818bac8d962f104
raboid <sean@dawnlabs.io>
2019-01-31 15:50:54 -0500

    responsive tweaks to hero

--- www/components/static-exporting/hero.js
+++ www/components/static-exporting/hero.js
@@ -9,7 +9,9 @@ const scrollToLinks = () =>
 
 export default () => (
   <Container wide padding center dotBackground>
-    <h1>Static Sites, No Compromise</h1>
+    <h1>
+      Static Sites, <br /> No Compromise
+    </h1>
 
     <div className="content">
       <p>
@@ -28,8 +30,28 @@ export default () => (
           font-size: 46px;
         }
 
+        h1 br {
+          display: none;
+        }
+
         .content {
-          margin: 3rem 0;
+          margin: 3rem 1rem;
+        }
+
+        @media screen and (max-width: 640px) {
+          h1 br {
+            display: initial;
+          }
+        }
+
+        @media screen and (max-width: 360px) {
+          h1 {
+            font-size: 40px;
+          }
+
+          p br {
+            display: none;
+          }
         }
       `}
     </style>

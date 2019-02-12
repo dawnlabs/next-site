3f31d4a8cb202fdcf34e971b71556a36f7b31053
raboid <sean@dawnlabs.io>
2019-02-11 19:21:58 -0500

    style example section

--- www/components/server-side-rendering/examples.js
+++ www/components/server-side-rendering/examples.js
@@ -1,14 +1,11 @@
 import Container from '../container';
-import SectionHeader from '../section-header'
 import Button from '../button'
 
 export default () => (
   <Container wide padding gray center>
-    <SectionHeader
-      title="Integrate Anywhere"
-    />
+    <h2 className="f0 fw6">Integrate Anywhere</h2>
 
-    <div>
+    <div className="content">
       <p>
         Need a path for incremental adoption? No problem. Server-side rendering with Next.js <br />
         can be easily integrated with existing application runtimes such as Express and Electron.
@@ -19,8 +16,27 @@ export default () => (
 
     <style jsx>
       {`
+        p {
+          margin: 0 1rem;
+        }
+
+        h2 {
+          margin: 0 0 1rem 0;
+          line-height: 1.3;
+        }
+
         .row {
           display: flex;
+        }
+
+        .content {
+          margin-bottom: 7.5rem;
+        }
+
+        @media screen and (max-width: 640px) {
+          br {
+            display: none;
+          }
         }
       `}
     </style>

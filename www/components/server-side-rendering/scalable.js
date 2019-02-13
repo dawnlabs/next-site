4b1fed4d35beeabdadf7ef45a15de9865724e4b9
raboid <sean@dawnlabs.io>
2019-02-12 20:11:46 -0500

    add asset to scalable section

--- www/components/server-side-rendering/scalable.js
+++ www/components/server-side-rendering/scalable.js
@@ -1,36 +1,36 @@
-import Container from '../container';
+import Container from '../container'
+import Lambda from './svg/lambda'
 
 export default () => (
   <Container wide padding gray>
     <div className="flex">
-      <div className="placeholder" />
+      <div className="lambda">
+        <Lambda />
+      </div>
 
       <div className="col">
-        <h2 className="f0 fw6">
-          Infinitely Scalable
-        </h2>
+        <h2 className="f0 fw6">Infinitely Scalable</h2>
 
         <div className="content">
           <p>
-            When it comes to dynamic applications, scalability is often a concern. Fortunately, Next.js supports serverless builds out of the box.
-            Simply set the target and Next.js will output an SSR-equipped lambda for each page which can be instantly deployed to platforms like Now 2.0.
+            When it comes to dynamic applications, scalability is often a concern. Fortunately, Next.js supports
+            serverless builds out of the box. Simply set the target and Next.js will output an SSR-equipped lambda for
+            each page which can be instantly deployed to platforms like Now 2.0.
           </p>
         </div>
       </div>
-
     </div>
     <style jsx>
       {`
-        .placeholder {
-          width: 200px;
-          height: 200px;
-          background: red;
+        h2 {
+          line-height: 1.3;
         }
 
         .flex {
+          position: relative;
           display: flex;
           align-items: center;
-          justify-content: space-between;
+          justify-content: flex-end;
           max-width: 1024px;
           margin: 0 auto;
           padding: 0 1rem;
@@ -42,10 +42,6 @@ export default () => (
           justify-content: center;
         }
 
-        h2 {
-          line-height: 1.3;
-        }
-
         .content {
           margin: 1rem 0 1.5rem;
           max-width: 25rem;
@@ -55,11 +51,9 @@ export default () => (
           .flex {
             justify-content: center;
           }
-
-          .placeholder {
+          .lambda {
             display: none;
           }
-
           .col {
             align-items: center;
             text-align: center;

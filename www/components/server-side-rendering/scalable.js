427a61b6516623f501e910eaa9955eb75fbdca5e
raboid <sean@dawnlabs.io>
2019-02-14 16:02:29 -0500

    use IObserver for scalable animation

--- www/components/server-side-rendering/scalable.js
+++ www/components/server-side-rendering/scalable.js
@@ -1,85 +1,104 @@
 import Container from '../container'
+import IObserver from '../intersection-observer'
 import Lambda from './svg/lambda'
 import { ExternalLink } from '../text/link'
 
-export default () => (
-  <Container wide padding gray>
-    <div className="flex">
-      <div className="lambda">
-        <Lambda />
-      </div>
+export default class Scalable extends React.PureComponent {
+  state = {
+    viewable: false
+  }
 
-      <div className="col">
-        <h2 className="f0 fw6">Infinitely Scalable</h2>
+  setViewable = ({ isIntersecting: viewable }) => viewable && this.setState({ viewable })
 
-        <div className="content">
-          <p>
-            When it comes to dynamic applications, scalability is often a concern. Fortunately, Next.js supports
-            serverless builds out of the box. Simply set the target and Next.js will output an SSR-equipped lambda for
-            each page which can be instantly deployed to platforms like{' '}
-            <ExternalLink href="https://zeit.co/blog/now-2">Now 2.0</ExternalLink>.
-          </p>
+  render() {
+    const { viewable } = this.state
+
+    return (
+      <Container wide padding gray>
+        <div className="flex">
+          <IObserver
+            onIntersect={this.setViewable}
+            rootMargin="0px 0px -250px 0px"
+            render={({ innerRef }) => (
+              <div className="lambda" ref={innerRef}>
+                {viewable && <Lambda />}
+              </div>
+            )}
+          />
+
+          <div className="col">
+            <h2 className="f0 fw6">Infinitely Scalable</h2>
+
+            <div className="content">
+              <p>
+                When it comes to dynamic applications, scalability is often a concern. Fortunately, Next.js supports
+                serverless builds out of the box. Simply set the target and Next.js will output an SSR-equipped lambda
+                for each page which can be instantly deployed to platforms like{' '}
+                <ExternalLink href="https://zeit.co/blog/now-2">Now 2.0</ExternalLink>.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-    <style jsx>
-      {`
-        h2 {
-          line-height: 1.3;
-          margin: 0 0 1rem 0;
-        }
+        <style jsx>
+          {`
+            h2 {
+              line-height: 1.3;
+              margin: 0 0 1rem 0;
+            }
 
-        p {
-          margin: 0;
-        }
+            p {
+              margin: 0;
+            }
 
-        .flex {
-          position: relative;
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          max-width: 1024px;
-          margin: 0 auto;
-          padding: 0 1rem;
-        }
+            .flex {
+              position: relative;
+              display: flex;
+              align-items: center;
+              justify-content: space-between;
+              max-width: 1024px;
+              margin: 0 auto;
+              padding: 0 1rem;
+            }
 
-        .col {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-        }
+            .col {
+              display: flex;
+              flex-direction: column;
+              justify-content: center;
+            }
 
-        .content {
-          z-index: 1;
-          max-width: 27rem;
-        }
+            .content {
+              z-index: 1;
+              max-width: 27rem;
+            }
 
-        .lambda {
-          height: 16rem;
-        }
+            .lambda {
+              height: 16rem;
+            }
 
-        @media screen and (max-width: 960px) {
-          .flex {
-            flex-direction: column-reverse;
-            align-items: center;
-          }
-          .col {
-            align-items: center;
-            text-align: center;
-          }
+            @media screen and (max-width: 960px) {
+              .flex {
+                flex-direction: column-reverse;
+                align-items: center;
+              }
+              .col {
+                align-items: center;
+                text-align: center;
+              }
 
-          .lambda {
-            margin: 4rem 3rem 0 0;
-            height: 16rem;
-          }
-        }
+              .lambda {
+                margin: 4rem 3rem 0 0;
+                height: 16rem;
+              }
+            }
 
-        @media screen and (max-width: 640px) {
-          .lambda {
-            transform: scale(0.85);
-          }
-        }
-      `}
-    </style>
-  </Container>
-)
+            @media screen and (max-width: 640px) {
+              .lambda {
+                transform: scale(0.85);
+              }
+            }
+          `}
+        </style>
+      </Container>
+    )
+  }
+}

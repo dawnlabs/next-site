8d45f2b6cebaf5a204403cb608cfe9d239bd8df3
raboid <sean@dawnlabs.io>
2019-02-25 15:02:53 -0500

    update docs section

--- www/components/css-in-js/docs.js
+++ www/components/css-in-js/docs.js
@@ -1,30 +1,30 @@
 import Container from '../container'
 import Button from '../button'
+import Hammer from './svg/hammer'
 
 export default () => (
   <Container wide padding gray>
     <div className="flex">
-      <div className="col">
-        <h2 className="f0 fw6">Great Developer Tooling</h2>
-        <div className="content">
-          <p>
-            Developing with <code>styled-jsx</code> is easier than ever with syntax highlighting for major editors like
-            VSCode, WebStorm and VSCode, directly within your components. And with a simple plugin system, it's simple
-            to use your favorite preprocessor like Sass, PostCSS, or Stylus.
-          </p>
-        </div>
-        <div>
-          <Button invert href="#">
-            View Documentation
-          </Button>
-        </div>
+      <Hammer />
+      <h2 className="f0 fw6">Great Developer Tooling</h2>
+      <div className="content">
+        <p>
+          Developing with <code>styled-jsx</code> is easier than ever with syntax highlighting for major editors like
+          VSCode, WebStorm and VSCode. With a simple plugin system, it's easy to use your favorite preprocessors such as
+          Sass, PostCSS, and Stylus.
+        </p>
       </div>
-
-      <div className="placeholder" />
+      <div>
+        <Button invert href="#">
+          View Documentation
+        </Button>
+      </div>
     </div>
+
     <style jsx>
       {`
         p {
+          text-align: center;
           margin: 0;
         }
 
@@ -33,47 +33,24 @@ export default () => (
         }
 
         h2 {
-          margin: 0 0 1.5rem 0;
+          margin: 2rem 0 1.5rem 0;
           line-height: 1.3;
         }
 
         .flex {
           display: flex;
-          align-items: center;
-          justify-content: space-between;
-          max-width: 1024px;
-          margin: 0 auto;
-          padding: 0 1rem;
-        }
-
-        .col {
-          display: flex;
           flex-direction: column;
-          justify-content: center;
+          align-items: center;
         }
 
         .content {
           margin: 0 0 2.5rem 0;
-          max-width: 28rem;
-        }
-
-        .placeholder {
-          width: 400px;
-          height: 300px;
-          background: red;
+          max-width: 38rem;
         }
 
         @media screen and (max-width: 960px) {
           h2 {
             margin-top: 1.5rem;
-          }
-          .flex {
-            justify-content: initial;
-            flex-direction: column-reverse;
-          }
-          .col {
-            align-items: center;
-            text-align: center;
           }
           .content {
             max-width: 27rem;
